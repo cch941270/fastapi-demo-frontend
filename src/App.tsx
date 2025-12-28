d6e91b22e1bd6bd7c14f392404cca43b0fc7d713
cch94 <cch1270@tutamail.com>
2025-12-28 23:32:50 +0800

    Fetch and render discussion threads

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import DiscussionThread from "./components/DiscussionThread.tsx";
+import {
+  type DiscussionThreadType, DiscussionThread,
+} from "./components/DiscussionThread.tsx";
 import Header from "./components/Header.tsx";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [discussionThreads, setDiscussionThreads] = useState<
+    DiscussionThreadType[]
+  >([]);
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    let ignore = false;
+    async function startFetching() {
+      const response = await fetch(`${apiUrl}/discussion_threads/`);
+      const data = await response.json();
+      if (!ignore) {
+        setDiscussionThreads(data);
+      }
+    }
+    startFetching();
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  const discussionThreadElements = discussionThreads.map((d) => (
+    <DiscussionThread key={d.id} discussionThread={d} />
+  ));
+
   return (
     <div className="max-w-[500px] mx-auto">
       <Header />
-      <DiscussionThread />
+      {discussionThreadElements}
     </div>
   );
 }
--- src/components/DiscussionThread.tsx
+++ src/components/DiscussionThread.tsx
@@ -1,10 +1,40 @@
-export default function DiscussionThread() {
+import imagePicture from "../assets/image-picture.svg";
+
+export type DiscussionThreadType = {
+  id: number;
+  title: string;
+  content: string;
+  author: string;
+  imagePath: string | null;
+  createdAt: Date;
+  updatedAt: Date | null;
+};
+
+export function DiscussionThread({
+  discussionThread,
+}: {
+  discussionThread: DiscussionThreadType;
+}) {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const imageSrc = discussionThread.imagePath
+    ? `${apiUrl}${discussionThread.imagePath}`
+    : imagePicture;
+  const createdAt = new Date(discussionThread.createdAt).toLocaleString();
+
   return (
-    <article className="grid grid-cols-10 grid-rows-4 p-[5px] my-[5px] bg-lime-50">
-      <div className="col-span-3 row-span-4">I am pic</div>
-      <div className="col-start-4 col-span-7 row-span-1">I am title</div>
-      <div className="col-start-4 col-span-7 row-start-2 row-span-3">
-        I am loooong content
+    <article className="grid grid-cols-10 grid-rows-4 h-[152px] p-[5px] my-[5px] bg-lime-50">
+      <div className="col-span-3 row-span-4">
+        <img className="w-[142px]" src={imageSrc} />
+      </div>
+      <div className="col-start-4 col-span-7 row-span-1 flex justify-between">
+        <span>{discussionThread.author}</span>
+        <span>{createdAt}</span>
+      </div>
+      <div className="col-start-4 col-span-7 row-start-2 row-span-1">
+        {discussionThread.title}
+      </div>
+      <div className="col-start-4 col-span-7 row-start-3 row-span-2 overflow-scroll">
+        {discussionThread.content}
       </div>
     </article>
   );
