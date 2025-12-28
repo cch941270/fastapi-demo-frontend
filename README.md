a13e6ba4981bbe6db232dfa9a5b968e6b414e113
cch94 <cch1270@tutamail.com>
2025-12-28 16:27:58 +0800

    Build header component

---
+++ README.md
@@ -0,0 +1,3 @@
+User Circle SVG
+Author: Solar Icons
+Source: https://www.svgrepo.com/svg/526700/user-circle
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import userCircle from "../assets/user-circle.svg";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 h-12 bg-amber-100">
+      <div className="absolute w-full h-full flex justify-center items-center">
+        FastAPI demo frontend
+      </div>
+      <div className="h-full flex justify-end items-center">
+        <img className="w-[40px]" src={userCircle} />
+      </div>
+    </header>
+  );
+}
