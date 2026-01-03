86c424647c0923b5ece5c2acd126e710cbc82d38
cch94 <cch1270@tutamail.com>
2026-01-03 22:10:26 +0800

    Implement logout function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,29 @@
 import userCircle from "../assets/user-circle.svg";
 import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 export default function Header() {
+  const tokenData = localStorage.getItem("accessToken");
+  let navigate = useNavigate();
+
+  function logout() {
+    localStorage.removeItem("accessToken");
+    navigate("/");
+  }
+
   return (
     <header className="sticky top-0 h-12 bg-amber-100">
       <div className="absolute w-full h-full flex justify-center items-center">
         FastAPI demo frontend
       </div>
-      <div className="relative h-full flex justify-end items-center">
+      <div className="relative h-full flex justify-between items-center">
+        {tokenData ? (
+          <button className="cursor-pointer" onClick={logout}>
+            Logout
+          </button>
+        ) : (
+          <span></span>
+        )}
         <Link to="/login">
           <img className="w-[40px]" src={userCircle} />
         </Link>
