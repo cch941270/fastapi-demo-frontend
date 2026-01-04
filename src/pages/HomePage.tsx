71007c00165f9b7d1a9731c9c0022ba43809ba29
cch94 <cch1270@tutamail.com>
2026-01-04 15:50:53 +0800

    Filter discussion threads in home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,11 +3,12 @@ import {
   DiscussionThread,
 } from "../components/DiscussionThread.tsx";
 import Header from "../components/Header.tsx";
-import { useState, useEffect } from "react";
+import { useState, useEffect, type ChangeEvent } from "react";
 
 export default function HomePage() {
   const [discussionThreads, setDiscussionThreads] =
     useState<DiscussionThreadType[]>([]);
+  const [titleFilter, setTitleFilter] = useState<string>("");
   const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
 
   useEffect(() => {
@@ -25,13 +26,28 @@ export default function HomePage() {
     };
   }, []);
 
-  const discussionThreadElements = discussionThreads.map((d) => (
-    <DiscussionThread key={d.id} discussionThread={d} />
-  ));
+  function filterDiscussionThreads(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.currentTarget;
+    setTitleFilter(value);
+  }
+
+  const discussionThreadElements = discussionThreads
+    .filter((d) => (titleFilter ? d.title.includes(titleFilter) : true))
+    .map((d) => <DiscussionThread key={d.id} discussionThread={d} />);
 
   return (
     <div>
       <Header />
+      <div className="h-10 flex items-center mt-[5px] bg-lime-100">
+        <input
+          type="text"
+          name="searchTitle"
+          placeholder="Search Title"
+          value={titleFilter}
+          onChange={filterDiscussionThreads}
+          className="w-full"
+        />
+      </div>
       {discussionThreadElements}
     </div>
   );
